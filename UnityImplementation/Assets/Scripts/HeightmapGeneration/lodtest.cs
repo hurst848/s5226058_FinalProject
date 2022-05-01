2e5b718b429cab65da620413c7d846a197c21240
Adam Hurst <adambbhurst@gmail.com>
2022-05-01 15:47:28 +0100

    TRying to figure out LOD

--- UnityImplementation/Assets/Scripts/HeightmapGeneration/lodtest.cs
+++ UnityImplementation/Assets/Scripts/HeightmapGeneration/lodtest.cs
@@ -12,6 +12,12 @@ public class lodtest : MonoBehaviour
 
     private MeshFilter filter;
 
+
+    private List<Vector3> MeshVerticies = new List<Vector3>();
+    private List<Vector2> MeshUVs = new List<Vector2>();
+    private List<int> MeshTriangles = new List<int>();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,44 +35,40 @@ public class lodtest : MonoBehaviour
         Vector3 AxisA = new Vector3(_normal.y, _normal.z, _normal.x);
         Vector3 AxisB = Vector3.Cross(_normal, AxisA);
 
-        
-
-        for (int i = 0; i < LOD; i++)
-        {
-            if (i + 1 != LOD)
-            {
-                float increment = 1.0f / (baseResolution + i);
-                for (int x = 0; x < baseResolution; x++)
-                {
-                    int itr = x * baseResolution;
-                    Vector2 percent = new Vector2(0, x) / (baseResolution - 1);
-                    Vector3 pointOnUnitCube = _normal + (percent.x - 0.5f) * 2 * AxisA + (percent.y - 0.5f) * 2 * AxisB;
-                    Vector3 pointOnUnitSphere = pointOnUnitCube * size;
-                    verticies.Add(pointOnUnitSphere);
-                    uvs.Add(new Vector2(x / baseResolution, 0 / baseResolution));
-                    if (x < baseResolution - 1)
-                    {
-                        triangles.Add(itr);
-                        triangles.Add(itr + (int)baseResolution);
-                        triangles.Add(itr + (int)baseResolution + 1);
-
-                        triangles.Add(itr);
-                        triangles.Add(itr + (int)baseResolution + 1);
-                        triangles.Add(itr + 1);
-
-
-                    }
-                }
-            }
-            else
-            {
-
-            }
-        }
 
 
 
         for (int y = 0; y < baseResolution; y++)
+        {
+            for (int x = 0; x < baseResolution; x++)
+            {
+
+                int itr = x + (y * (int)baseResolution);
+                Vector2 percent = new Vector2(y, x) / (baseResolution - 1);
+                Vector3 pointOnUnitCube = _normal + (percent.x - 0.5f) * 2 * AxisA + (percent.y - 0.5f) * 2 * AxisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube * size;
+                verticies.Add(pointOnUnitSphere);
+                uvs.Add(new Vector2(x / baseResolution, y / baseResolution));
+                if (x < baseResolution - 1 && y < baseResolution - 1)
+                {
+                    triangles.Add(itr);
+                    triangles.Add(itr + (int)baseResolution);
+                    triangles.Add(itr + (int)baseResolution + 1);
+
+
+                    triangles.Add(itr);
+                    triangles.Add(itr + (int)baseResolution + 1);
+                    triangles.Add(itr + 1);
+
+                }
+            }
+        }
+            
+    
+
+
+
+       /* for (int y = 0; y < baseResolution; y++)
         {
             for (int x = 0; x < baseResolution; x++)
             {
@@ -90,7 +92,7 @@ public class lodtest : MonoBehaviour
 
                 }
             }
-        }
+        }*/
         Mesh m = new Mesh();
         
         m.SetVertices(verticies);
@@ -102,6 +104,26 @@ public class lodtest : MonoBehaviour
 
 
     }
+
+
+    private void GenerateMesh()
+    {
+        
+
+
+
+        for (int y = 0; y < 2; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+
+            }
+        }
+        
+    }
+
+
+
 
     // https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
     private float map(float _value, float _fromA, float _ToA, float _fromB, float _ToB)
